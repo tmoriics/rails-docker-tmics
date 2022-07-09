efdc04a45260c6813e544309306ee4a3950ce6e6
Taketoshi MORI <tmoriics-tky@umin.ac.jp>
2022-07-09 11:07:18 +0900

    CircleCI configuration

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert false
+  end
 end
+
